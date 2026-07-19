f6ac4b7237e8fa643a12924158e2c0fcf772dd4d
Jack Li <linb.net@gmail.com>
2026-07-19 18:55:47 -0400

    feat: update files

--- Card.jsx
+++ Card.jsx
@@ -13,7 +13,7 @@ import {OpenInNew} from '@mui/icons-material';
  *   description  {string}
  *   bgcolor      {string}   Background colour for the card header band
  *   selected     {boolean}
- *   onSelect     {Function} dbac
+ *   onSelect     {Function}
  */
 export default function Card({ title, description, bgcolor = '#f5f5f5', selected, onSelect }) {
     return (
